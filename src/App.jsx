b39a1cd4a4242f34dce0069514e1118040e96f62
kanikarawlo <rawlokanika05@gmail.com>
2025-04-09 19:15:52 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
     setError(null);
 
     try {
-      const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+      const apiKey = import.meta.env.OPENWEATHER_API;
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
       );
